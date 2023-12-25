8f92c6484940263e5c3317d7562e7c2ca62ee891
Src77 <ideamanhh@naver.com>
2023-12-25 17:23:39 +0900

    8-1

--- C/step/8/2745.cpp
+++ C/step/8/2745.cpp
@@ -1,4 +1,19 @@
 #include <cstdio>
+#include <cstring>
+#include <cctype>
+char N[33];
+int B, D, i;
 int main(){
-    
+    scanf("%s", N);
+    scanf("%d", &B);
+    for(i=0;i<strlen(N);i++){
+        D *= B;
+        if(isalpha(N[i])){
+            D += N[i] - 'A' + 10;
+        }
+        else{
+            D += N[i] - '0';
+        }
+    }
+    printf("%d", D);
 }
